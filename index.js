ef6174266be90a0321aa26f2982178e5412a405b
farazkhan2062 <farazkhan2062@gmail.com>
2024-04-17 23:59:29 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+// improt a 'inquirer' module, which is the command line interfaace for node.js
+import inquirer from "inquirer";
+//Declear a Constant "answer" and assign ti the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence or pragraph: "
+    },
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(`Your sentence has ${words.length} words`);
+console.log(`Your sentence has ${answers.sentence.length} characters`);
+console.log(`Your sentence has ${answers.sentence.trim().length} characters without spaces`);
+console.log(`Your sentence has ${answers.sentence.trim().split(" ").length} words`);
+console.log(`Your sentence has ${answers.sentence.trim().split(" ").length} words without spaces`);
+console.log(words);
